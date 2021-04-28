990c4af2e6c6c0495048aad3831eb13b0e4ddf50
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-04-28 08:11:30 -0500

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,8 @@
+if (window.location.search == "?l=es") {
+  location.replace("https://lb123658.github.io/goat/es");
+} else if (window.location.search == "?l=pl") {
+  location.replace("https://lb123658.github.io/goat/pl");
+} else {
+  location.replace("https://lb123658.github.io/goat/app");
+}
+  
