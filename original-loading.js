99067657391f406987273dc1fb88f843f7c6465d
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-04-28 14:14:47 -0500

    Update original-loading.js

--- original-loading.js
+++ original-loading.js
@@ -1,3 +1,21 @@
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("play");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+function jump() {
+document.getElementById("goat").style.top = "180px";
+}
+function gravity() {
+document.getElementById("goat").style.top = "218px";
+}
 function loadA() {
 document.getElementById("a").style.background = "#63bd04";
 document.getElementById("loadPercent").innerHTML = "Loading 25%";
