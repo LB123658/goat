5648c5cc86843932e143486c73dd21973d5f7f9f
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-04-28 14:14:08 -0500

    Create original-loading.js

---
+++ original-loading.js
@@ -0,0 +1,20 @@
+function loadA() {
+document.getElementById("a").style.background = "#63bd04";
+document.getElementById("loadPercent").innerHTML = "Loading 25%";
+}
+function loadB() {
+document.getElementById("b").style.background = "#63bd04";
+document.getElementById("loadPercent").innerHTML = "Loading 50%";
+}
+function loadC() {
+document.getElementById("c").style.background = "#63bd04";
+document.getElementById("loadPercent").innerHTML = "Loading 75%";
+}
+function loadD() {
+document.getElementById("d").style.background = "#63bd04";
+document.getElementById("loadPercent").innerHTML = "Loading 100%";
+}
+function finalLoad() {
+document.getElementById("setOne").style.visibility = "hidden";
+document.getElementById("setTwo").style.visibility = "visible";
+}
